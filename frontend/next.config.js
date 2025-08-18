ecdcf1aafe20985cb26ff9f8d3468e7d740e92af
SaurabhYadav45 <saurabhkry08@gmail.com>
2025-08-18 10:26:19 +0530

    renamed next.config.ts to next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your Next.js configuration options go here.
+  // For now, an empty object is perfectly fine.
+};
+
+module.exports = nextConfig;
